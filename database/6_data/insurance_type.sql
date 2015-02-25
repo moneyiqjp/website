d4d15f5ca42ab262fd6551432c7b0d72240db8d9
benfries <benfries@gmail.com>
2015-02-25 21:20:01 +0900

    Data entry 3
    Step by step cleaning up the data ...

---
+++ database/6_data/insurance_type.sql
@@ -0,0 +1,14 @@
+insert into insurance_type (type_name, subtype_name, description, region, update_time, update_user) values
+
+'travel', 'travel', 'domestic'
+'travel', 'death', 'domestic'
+'travel', 'hospital', 'domestic'
+'travel', 'travel', 'international'
+'travel', 'death', 'international'
+'travel', 'hospital', 'international'
+'shopping', 'shopping', 'domestic'
+domestic_travel 	 domestic_travel_death 	 domestic_travel_hospital 	 
+international_travel 	 
+international_travel_death 	 
+international_travel_hospital 	 
+international_travel_luggage 
